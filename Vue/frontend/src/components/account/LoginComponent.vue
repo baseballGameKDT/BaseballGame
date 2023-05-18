646c33b3b02ac780c552818981b8c640c08d69ce
Hakgyu Kim <zage4448@gmail.com>
2023-05-18 13:55:26 +0900

    feat: [김학규] 로그인 기능 구현 [VBG-5]

---
+++ Vue/frontend/src/components/account/LoginComponent.vue
@@ -0,0 +1,43 @@
+<template lang="">
+  <div>
+    <v-btn v-if="!isLoggedIn" @click="readyToLogin">로그인</v-btn>
+    <div v-if="isReadyToLogin">
+      <label> 이메일: <input type="text" v-model="email" required /> </label
+      ><br />
+      <label>
+        비밀번호: <input type="password" v-model="password" required /> </label
+      ><br />
+      <v-btn @click="login">로그인</v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      isReadyToLogin: false,
+      isLoggedIn: false,
+    };
+  },
+  methods: {
+    login() {
+      const { email, password } = this;
+      this.$emit("click", { email, password });
+      this.isReadyToLogin = false;
+    },
+    readyToLogin() {
+      this.isReadyToLogin = true;
+    },
+  },
+  mounted() {
+    if (localStorage.getItem("loginUserId") == null) {
+      this.isLoggedIn = false;
+    } else {
+      this.isLoggedIn = true;
+    }
+  },
+};
+</script>
+<style lang=""></style>
--- Vue/frontend/src/views/HomeView.vue
+++ Vue/frontend/src/views/HomeView.vue
@@ -1,22 +1,49 @@
 <template lang="">
   <div align="center">
-    <router-link :to="{ name: 'SignUpPage'}">
+    <router-link :to="{ name: 'SignUpPage' }">
       <v-btn color="primary">회원 가입</v-btn>
-    </router-link> <br>
-    <router-link :to="{ name: 'GameLevelPage'}">
-      <v-btn color="primary">게임 플레이</v-btn>
     </router-link>
+    <LoginComponent @click="login" /><br />
+    <div v-if="isLoggedIn">
+      {{ nickName }} 님 어서오세요!
+      <router-link :to="{ name: 'GameLevelPage' }">
+        <v-btn color="primary">게임 플레이</v-btn>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import LoginComponent from "@/components/account/LoginComponent.vue";
+import { mapActions } from "vuex";
+
+const accountModule = "accountModule";
 
 export default {
-
-}
-
+  data() {
+    return {
+      isLoggedIn: false,
+      nickName: "",
+    };
+  },
+  components: {
+    LoginComponent,
+  },
+  methods: {
+    ...mapActions(accountModule, ["requestLoginToSpring"]),
+    async login(payload) {
+      await this.requestLoginToSpring(payload);
+    },
+  },
+  mounted() {
+    if (localStorage.getItem("loginUserId") == null) {
+      this.isLoggedIn = false;
+    } else {
+      this.isLoggedIn = true;
+      this.nickName = localStorage.getItem("loginUserNickName");
+    }
+  },
+};
 </script>
 
-<style lang="">
-  
-</style>
+<style lang=""></style>
