9171a7e18bf38091c2f688de78b397aa9e3a0820
김학규 <128335564+zage4448@users.noreply.github.com>
2023-05-19 09:15:50 +0900

    feat: [김학규] 로그인 및 회원 가입 ui [VBG-5,6] (#5)
    * update: [김학규] 숫자 5개, 5턴 옵션 삭제 [VBG-1]
    
    * update: [김학규] 0 선택지 추가 [VBG-2]
    
    * update: [김학규] 불필요 js 삭제
    
    * feat: [김학규] 회원 가입 구현 완료 [VBG-4]
    
    * feat: [김학규] 로그인 기능 구현 [VBG-5]
    
    * update: [김학규] 로그인 후 게임 난이도 설정 페이지로 가게 수정 [VBG-5]

---
+++ Vue/frontend/src/components/account/LoginComponent.vue
@@ -0,0 +1,35 @@
+<template lang="">
+  <div>
+    <v-btn @click="readyToLogin">로그인</v-btn>
+    <div v-if="isReadyToLogin">
+      <label> 이메일: <input type="text" v-model="email" required /> </label
+      ><br />
+      <label>
+        비밀번호: <input type="password" v-model="password" required /> </label
+      ><br />
+      <v-btn @click="login">로그인</v-btn>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      isReadyToLogin: false,
+    };
+  },
+  methods: {
+    login() {
+      const { email, password } = this;
+      this.$emit("click", { email, password });
+      this.isReadyToLogin = false;
+    },
+    readyToLogin() {
+      this.isReadyToLogin = true;
+    },
+  },
+};
+</script>
+<style lang=""></style>
--- Vue/frontend/src/views/HomeView.vue
+++ Vue/frontend/src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <template lang="">
   <div align="center">
-    <router-link :to="{ name: 'GameLevelPage'}">
-      <v-btn color="primary">게임 플레이</v-btn>
+    <router-link :to="{ name: 'SignUpPage' }">
+      <v-btn color="primary">회원 가입</v-btn>
     </router-link>
+    <LoginComponent @click="login" />
+    </div>
   </div>
 </template>
 
 <script>
+import LoginComponent from "@/components/account/LoginComponent.vue";
+import { mapActions } from "vuex";
+
+const accountModule = "accountModule";
 
 export default {
-
-}
-
+  components: {
+    LoginComponent,
+  },
+  methods: {
+    ...mapActions(accountModule, ["requestLoginToSpring"]),
+    async login(payload) {
+      await this.requestLoginToSpring(payload);
+      await this.$router.push({
+        name: 'GameLevelPage'
+      })
+    },
+  },
+};
 </script>
 
-<style lang="">
-  
-</style>
+<style lang=""></style>
