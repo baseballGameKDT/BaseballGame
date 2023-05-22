45f16522b789975246933b2d975fe4f7664aeb9c
김학규 <zage4448@gmail.com>
2023-05-22 22:09:09 +0900

    update: [김학규] div 설정 추가

--- Vue/frontend/src/components/account/SignUpComponent.vue
+++ Vue/frontend/src/components/account/SignUpComponent.vue
@@ -1,42 +1,35 @@
 <template lang="">
-    <div>
-        <form @submit.prevent="createAccount">
-            <label>
-                이메일: <input type="text" v-model="email" required/>
-            </label><br/>
-            <label>
-                닉네임: <input type="text" v-model="nickname" required/>
-            </label><br/>
-            <label>
-                비밀번호: <input type="password" v-model="password" required/>
-            </label><br/>
-            <div>
-                <v-btn color="primary" type="submit">가입</v-btn>
-                <router-link :to="{ name: 'home' }">
-                    취소
-                </router-link>
-            </div>
-        </form>
-    </div>
+  <div>
+    <form @submit.prevent="createAccount">
+      <label> 이메일: <input type="text" v-model="email" required /> </label
+      ><br />
+      <label> 닉네임: <input type="text" v-model="nickname" required /> </label
+      ><br />
+      <label>
+        비밀번호: <input type="password" v-model="password" required /> </label
+      ><br />
+      <div align="center">
+        <v-btn color="primary" type="submit">가입</v-btn>
+        <router-link :to="{ name: 'home' }"> 취소 </router-link>
+      </div>
+    </form>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-        return {
-            email: '',
-            nickname: '',
-            password: ''
-        }
+  data() {
+    return {
+      email: "",
+      nickname: "",
+      password: "",
+    };
+  },
+  methods: {
+    createAccount() {
+      const { email, nickname, password } = this;
+      this.$emit("submit", { email, nickname, password });
     },
-    methods: {
-        createAccount () {
-            const { email, nickname, password } = this
-            this.$emit('submit', { email, nickname, password })
-        }
-    }
-    
-}
+  },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
--- Vue/frontend/src/views/HomeView.vue
+++ Vue/frontend/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template lang="">
-  <div align="center">
+  <div>
     <router-link :to="{ name: 'SignUpPage' }">
       <v-btn v-if="!isLoggedIn" color="primary">회원 가입</v-btn>
     </router-link>
     <LoginComponent @click="login" /><br />
-    <router-link :to="{ name: 'GameLevelPage' }">
-      <v-btn v-if="isLoggedIn" color="primary">게임 플레이</v-btn>
-    </router-link>
+    <div v-if="isLoggedIn">
+      <router-link :to="{ name: 'GameLevelPage' }">
+        <v-btn color="primary">게임 플레이</v-btn>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -37,9 +39,18 @@ export default {
       this.isLoggedIn = false;
     } else {
       this.isLoggedIn = true;
+      this.nickname = localStorage.getItem("loginUserNickName");
     }
   },
 };
 </script>
 
-<style lang=""></style>
+<style scoped>
+div {
+  margin: 30px 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
